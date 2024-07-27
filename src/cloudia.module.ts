fa4c436c0837593433b2f6214750545c8bf0e2ea
Melwin Kramer <melwin.kramer@gmail.com>
2024-07-27 23:49:17 +0200

    feat: added port customization and changed name to cloudia

--- src/cloudia.module.ts
+++ src/cloudia.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './controllers/app.controller';
-import { AppService } from './services/app.service';
 import { PrismaService } from './services/prisma.service';
 import { LoginController } from './controllers/login.controller';
 import { LoginService } from './services/login.service';
@@ -11,4 +9,4 @@ import { UserService } from './services/user.service';
   controllers: [LoginController],
   providers: [PrismaService, UserService, LoginService],
 })
-export class AppModule {}
+export class CloudiaModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { CloudiaModule } from './cloudia.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(8000);
+  const app = await NestFactory.create(CloudiaModule);
+  await app.listen(process.env.PORT || 8000) // switch to port 8000 if no alternative is given
 }
 bootstrap();
