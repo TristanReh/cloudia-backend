f413c6ae05208caf0ae920e61a845158fd397b4a
Tristan Reh <rehtristan@gmail.com>
2024-07-31 21:09:01 +0200

    added middleware
    Co-authored-by: Melwin Kramer <Melwiin@users.noreply.github.com>

---
+++ src/middlewares/authentication.middleware.ts
@@ -0,0 +1,29 @@
+import { BadRequestException, createParamDecorator, ExecutionContext, ForbiddenException, Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Response, Request} from 'express';
+import { TokenExpiredError, verify } from 'jsonwebtoken';
+
+
+export const TokenEmail = createParamDecorator(
+  (_data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.tokenEmail; // extract token from request
+  },
+);
+
+@Injectable()
+export class AuthenticationMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    let accessToken = req.cookies["access_token"];
+    if (!accessToken) throw new BadRequestException("AccessToken was not provided");
+    
+    verify(accessToken, process.env.JWT_SECRET_KEY, {}, (err, decoded) => {
+        if (err) {
+          if (err instanceof TokenExpiredError) throw new ForbiddenException("Access token is expired");
+          throw new BadRequestException("Couldn't verify access token")
+        }
+        req["tokenEmail"] = decoded["email"];
+    });
+    
+    next();
+  }
+}
