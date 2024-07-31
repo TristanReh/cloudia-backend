33134541b05d6cdba1dae1b637606f3eb42b6e0a
Tristan Reh <rehtristan@gmail.com>
2024-07-31 21:10:42 +0200

    fixed cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { CloudiaModule } from './cloudia.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(CloudiaModule);
+  app.enableCors({
+    origin: "http://192.168.2.177:5173",
+    credentials: true
+  })
+  app.use(cookieParser())
   await app.listen(process.env.PORT || 8000) // switch to port 8000 if no alternative is given
 }
+
 bootstrap();
