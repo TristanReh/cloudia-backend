e5435363db23222ee46d490a7aec307a25056a9b
Tristan Reh <rehtristan@gmail.com>
2024-07-31 21:09:25 +0200

    finished login.controller

--- src/controllers/login.controller.ts
+++ src/controllers/login.controller.ts
@@ -1,24 +1,28 @@
-import { BadRequestException, Controller, Get, Post, Req, UnauthorizedException } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Post, Req, Res, UnauthorizedException } from '@nestjs/common';
 import { LoginService } from '../services/login.service';
+import { Response } from 'express';
 
 @Controller('login')
 export class LoginController {
     constructor(private readonly loginService: LoginService) {}
 
     @Get()
-    async login(@Req() request: Request) : Promise<{ access_token: string }> {
-        let email : string = request.headers["email"]
-        let password : string = request.headers["password"]
+    async login(@Req() request: Request, @Res({passthrough: true}) response : Response) : Promise<void> {
+        let email : string = request.headers["email"];
+        let password : string = request.headers["password"] || "";
 
-        if (!email || !password) {
+        if (!email) {
             throw new BadRequestException('Please provide both email and password');
         }
       
         try {
             await this.loginService.checkCredentials(email, password);
-            return await this.loginService.generateToken(email);
+            var payload = await this.loginService.generateToken(email);
         } catch (err) {
             throw err;
         }
+
+        response.cookie("access_token", payload.access_token);
     }
 }
+
